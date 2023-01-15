32e565132c0f4dbee50013b08ec8fd71c5fa980b
ivan <ivannovaa.24@gmail.com>
2023-01-15 19:05:47 +0700

    fetch http blog & blog detail page

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,35 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Blog = () => {
   const navigate = useNavigate();
+  const [data, setData] = useState();
+  async function getPosts() {
+    await axios
+      .get("https://dummyjson.com/posts")
+      .then((response) => setData(response.data.posts));
+  }
+  useEffect(() => {
+    getPosts();
+  }, []);
+  console.log(data);
   return (
     <>
       <main>
         <div className="containers py-3 px-3 md:px-10">
           <div className="grid grid-cols-12 gap-5">
-            {[1, 2, 3, 4, 5].map((item) => (
-              <div className="col-span-12 md:col-span-3">
+            <div className="col-span-12 md:col-span-8 p-3">
+              {data?.map((item) => (
                 <div
-                  className="bg-white border cursor-pointer"
-                  onClick={() => navigate(`/blog/${item}`)}
+                  className="bg-white border cursor-pointer mb-3"
+                  onClick={() => navigate(`/blog/${item.id}`)}
                 >
-                  <div className="bg-slate-500 w-full h-[200px]"></div>
+                  {/* <div className="bg-slate-500 w-full h-[200px]"></div> */}
                   <div className="p-3">
                     <h5 className="text-slate-500 font-semibold">
-                      Lorem ipsum dolor sit amet.
+                      {item.title}
                     </h5>
                     <p className="text-slate-400 text-sm">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      Natus ducimus veniam libero reprehenderit tenetur aliquid
-                      in veritatis ab voluptates exercitationem! Doloribus
-                      temporibus voluptates accusamus ipsa ea molestias culpa
-                      vero laborum.
+                      {item.body.split(" ").splice(0, 20).join(" ")}
                     </p>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="hidden col-span-0 md:block md:col-span-4"></div>
           </div>
         </div>
       </main>
--- src/pages/BlogDetail/index.jsx
+++ src/pages/BlogDetail/index.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const BlogDetail = () => {
+  const blogId = useParams();
+  const [data, setData] = useState();
+  async function getDetailPost(id) {
+    await axios
+      .get("https://dummyjson.com/posts/1")
+      .then((response) => setData(response.data));
+  }
+  useEffect(() => {
+    getDetailPost(blogId);
+  }, [blogId]);
   return (
     <>
       <main className="pt-3">
         <div className="containers !max-w-5xl border">
-          <div className="bg-slate-900 w-full h-[450px]"></div>
           <div className="blog-content px-3 mt-3">
-            <h1 className="font-semibold text-lg md:text-3xl">
-              Lorem ipsum dolor sit amet.
-            </h1>
-            <p className="text-sm text-slate-500">Ivan Nova Ardiansyah</p>
-            <p className="text-sm text-slate-500">15 January 2023</p>
+            <h1 className="font-semibold text-lg md:text-3xl">{data?.title}</h1>
+            <div className="flex gap-3">
+              {data?.tags.map((item) => (
+                <p className="text-sm text-slate-500">{item}</p>
+              ))}
+            </div>
+            <p className="text-sm text-slate-500 mt-2">{data?.body}</p>
           </div>
         </div>
       </main>
