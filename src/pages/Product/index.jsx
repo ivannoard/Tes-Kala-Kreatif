2a4dce58002b7d95d67cd9f5d418de3d461d3e25
ivan <ivannovaa.24@gmail.com>
2023-01-16 13:29:40 +0700

    clean code

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { BsChevronDown } from "react-icons/bs";
-import { Card, CardSkeleton, TextSkeleton } from "../../components/global";
+import { Card, CardSkeleton } from "../../components/global";
 import {
   CategoryHeader,
   CategorySidebar,
